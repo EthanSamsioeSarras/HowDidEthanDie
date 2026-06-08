1cf5c4cb6b6714d8cf0221f62e5a93097d498fc2
Ethan Samsioe Sarras <ethan.samsioesarra@gmail.com>
2026-06-08 15:43:03 +0200

    Unreal Project Creation
    +Added the project
    +Modified the gitignore

---
+++ HowDidEthanDie/Source/HowDidEthanDie/HowDidEthanDie.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HowDidEthanDie.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HowDidEthanDie, "HowDidEthanDie" );
